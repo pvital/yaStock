6a55153823ceff65e0a99649e42940f0413e7068
Paulo Vital <paulo@vital.eng.br>
2021-01-26 11:15:51 +0100

    Wallet - Adding service to manage wallets.
    Signed-off-by: Paulo Vital <paulo@vital.eng.br>

---
+++ src/wallet/wallet.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# yaStock - yet another Stock Wallet Tracking.
+# Copyright (c) 2021 Paulo Vital <paulo@vital.eng.br>
+#
+
+
+def main():
+    wallets = []
+    print(f'Currently you have {len(wallets)} wallets.')
+
+if __name__ == '__main__':
+    main()
